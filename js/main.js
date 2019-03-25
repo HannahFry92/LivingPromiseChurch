680887aa36dd6c5522b267c9233d4d10138bf2e9
lifestyleismine <33406019+lifestyleismine@users.noreply.github.com>
2019-03-25 18:41:07 -0400

    update easter time

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@
   //   );
   // });
 
-  $('#date-countdown').countdown('2019/09/28 16:00:00', function(event) {
+  $('#date-countdown').countdown('2019/04/21 10:45:00', function(event) {
   var $this = $(this).html(event.strftime(''
     + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
     + '<span class="countdown-block"><span class="label">%d</span> days </span>'
