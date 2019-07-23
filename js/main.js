e648bfcea5e61dcf643e362006f27d1614c337db
HannahFry92 <33818163+HannahFry92@users.noreply.github.com>
2019-07-23 13:35:21 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@
   //   );
   // });
 
-  $('#date-countdown').countdown('2019/09/13 19:30:00', function(event) {
+  $('#date-countdown').countdown('2019/07/26 17:30:00', function(event) {
   var $this = $(this).html(event.strftime(''
     + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
     + '<span class="countdown-block"><span class="label">%d</span> days </span>'
